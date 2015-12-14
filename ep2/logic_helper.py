259d6d706ad9dce939cc4f36358a343911da9c98
nathanPro <nathan@vieiraproenca.com>
2015-12-14 17:22:35 +0000

    Pre imagem fraca funcionando

---
+++ ep2/logic_helper.py
@@ -0,0 +1,18 @@
+from pyeda.inter  import *
+from functools    import reduce
+
+def negation(list_of_literals):
+    return list(map(lambda a: ~a, list_of_literals))
+
+def conjunction(list_of_literals):
+    return reduce(lambda a,b: a & b, list_of_literals)
+
+def disjunction(list_of_literals):
+    return reduce(lambda a,b: a | b, list_of_literals)
+
+def exists(bdd, bdd_vars):
+    ans = bdd
+    n = len(bdd_vars)
+    for i in range(n):
+        ans = ans.restrict({bdd_vars[i,1] : 0}) | ans.restrict({bdd_vars[i,1] : 1})
+    return ans
---
+++ ep2/main.py
@@ -0,0 +1,58 @@
+from pyeda.inter  import *
+from parser       import CTLtree
+from functools    import reduce
+from logic_helper import *
+import re
+
+# Lê entrada e remove espaços em branco
+n      = int(input())
+edges  = re.sub('[\s+]', '', input())
+labels = re.sub('[\s+]', '', input())
+
+# Lista ordenada das variaveis que ocorreram na entrada
+variables = list(set(re.sub('[\[\]\(\)"]', '', labels).split(",")))
+variables.sort()
+# Transicoes de estados
+edges  = edges[2:-2].split("),(")
+# Conjunto de labels que sao verdadeiras em cada estado
+labels = labels[2:-2].split("),(")
+labels = list(map(lambda label: set(label[1:-1].split('","')), labels))
+
+x = bddvars('x', len(variables), 2)
+
+# Lista de estados s e suas versoes s'
+S  = []
+pS = []
+for label in labels:
+    symbols = []
+    prime_symbols = []
+    for i in range(len(variables)):
+        if(variables[i] in label):
+            symbols.append(x[i,0])
+            prime_symbols.append(x[i,1])
+        else:
+            symbols.append(~x[i,0])
+            prime_symbols.append(~x[i,1])
+    S.append(conjunction(symbols))
+    pS.append(conjunction(prime_symbols))
+
+# Representacao do modelo
+model = []
+for edge in edges:
+    u, v = map(int, edge.split(","))
+    model.append(S[u] & pS[v])
+model = disjunction(model)
+
+# Le estados os quais devemos procurar pre imagem
+tgt = map(int,input().split())
+B = disjunction(list(map(lambda u: pS[u], tgt)))
+
+# Calcula pre imagem
+pre = exists(B & model, x)
+
+# Imprime lista de estados na pre imagem
+ans = []
+for u in range(len(S)):
+    if((S[u] & ~pre).is_zero()): # Testa se estado contido
+        ans.append(u)
+print(ans)
